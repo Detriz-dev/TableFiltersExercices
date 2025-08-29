8679d7404bb867089c700420eb70f33c95cc7e0e
Detriz-dev <kowalczyk.dawid@gmail.com>
2025-08-29 20:42:03 +0200

    CompaniesData page added

--- src/TableComponent.tsx
+++ src/TableComponent.tsx
@@ -7,6 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import CompaniesData from "./CompaniesData.tsx"
+
 function createData(
   name: string,
   calories: number,
