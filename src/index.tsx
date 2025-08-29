dcdbebe94a708929ac38ed0a65331dbf4faa919a
Detriz-dev <kowalczyk.dawid@gmail.com>
2025-08-29 18:00:03 +0200

    Table Component

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import {SelectDropDown} from './SelectDropDown';
+import TableComponent from  './TableComponent'
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.querySelector("#root")!).render(
 
       <div><SelectDropDown label="elo" placeholder="elo2" /></div>
       <div><SelectDropDown label="elo" placeholder="elo2" /></div>
+      <div><TableComponent /> </div>
      
     </StyledEngineProvider>
   </React.StrictMode>
